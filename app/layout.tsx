65ae1240901877e84b591955e68ce54e4b4a47e7
filipto861 <120219642+filipto861@users.noreply.github.com>
2026-08-24 13:11:49 +0200

    v1.12 wire PWA metadata and standalone viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,23 @@ import "./globals.css";
 import "./v082.css";
 import "./v083.css";
 import "./v107.css";
+import "./v112.css";
 import "leaflet/dist/leaflet.css";
 
 export const metadata: Metadata = {
   title: "FlyTally",
   description: "Digital pilot logbook",
+  applicationName:"FlyTally",
+  manifest:"/manifest.webmanifest",
   icons:{icon:"/logbook_icon_32.png",shortcut:"/logbook_icon_32.png",apple:"/logbook_icon.png"},
+  appleWebApp:{capable:true,statusBarStyle:"black-translucent",title:"FlyTally"},
+  formatDetection:{telephone:false},
 };
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  viewportFit:"cover",
   colorScheme: "dark",
   themeColor: "#07111f",
 };
